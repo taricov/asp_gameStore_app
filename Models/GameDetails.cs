5ace0d78702579fbd71bff4faa51481a1135c373
TaricOv <tareq.ouf@gmail.com>
2024-07-04 10:47:00 +0300

    added validation the form field

--- Models/GameDetails.cs
+++ Models/GameDetails.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Models;
 
 public class GameDetails
 {
     public int Id { get; set;}
+    [Required]
+    [StringLength(10)]
     public required string Name { get; set;}
+    [Range(10, 100)]
     public decimal Price { get; set;}
+    [Required]
     public string? GenreId { get; set;} 
     public DateOnly ReleaseDate { get; set; }    
 
